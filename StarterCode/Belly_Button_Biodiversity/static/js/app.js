6809b124e41c5cd3fea64e86df4e12d0393aff41
Rottvan <rottvan@gmail.com>
2019-06-13 23:46:03 -0700

    updating file

--- StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -1,137 +1,73 @@
 function buildMetadata(sample) {
-  // console.log(sample);
-  // @TODO: Complete the following function that builds the metadata panel
 
-  // Use `d3.json` to fetch the metadata for a sample
-    // Use d3 to select the panel with id of `#sample-metadata`
-  var panelId = d3.select('#sample-metadata');
-    // Use `.html("") to clear any existing metadata
-  panelId.html("");
-  d3.json("metadata/"+sample).then((sampleMData) => {
-    Object.entries(sampleMData).forEach(([key,value])=>{
-      panelId.append('p').text(`${key}: &{value}`)
-    });
-  });
-    // Use `Object.entries` to add each key and value pair to the panel
+  // @TODO: Complete the following function that builds the metadata panel
+  // hard code a sample id to see how it works
+  // look at Saturday fullstack flask Plotly, stu stocks dynamic
+
+    d3.json(`/metadata/${sample}`).then((data) => { // Use `d3.json` to fetch the metadata for a sample
+      var samplepanel = d3.select("#sample-metadata"); // Use d3 to select the panel with id of `#sample-metadata`
+      samplepanel.html("") // Use `.html("") to clear any existing metadata
+      Object.entries(data).forEach(([key, value]) => { // Use `Object.entries` to add each key and value pair to the panel
+      samplepanel.append("p").text(`${key} : ${value}`);
+      });
+     });
+   }
+
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
 
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
-}
 
 function buildCharts(sample) {
-  d3.json("samples/"+sample).then((sampleData)=>{
-    // bubble chart
-    var trace1 = {
-      x:sampleData['otu_ids'],
-      y:sampleData['sample_values'],
-      mode: 'markers',
-      type: 'scatter',
-      text: sampleData['otu_labels'],
-      marker:{
-        color: sampleData['otu_ids'],
-        opacity: 0.5,
-        size: sampleData['sample_values']
-      }
-    };
-    // piechart
-    var data2=[{
-      "labels":sampleData['otu_ids'].slice(0,10),
-      "values":sampleData['sample_values'].slice(0,10),
-      "type": 'pie',
-      "hovertext":sampleData['otu_labels']
-    }]
-    //bubble
-    var data=[trace1];
-    // piechart
-    var layout={
-      title: `bubble chart for ${sample}`,
-      xaxis: {title:'otu IDs'},
-      yaxis: {title:'Sample Values'}
-    };
-    var layout2 = {title: `Pie Chart for ${sample}`};
-    Plotly.newPlot('bubble', data, layout);
-    Plotly.newPlot('pie', data2, layout2);
 
+  // @TODO: Use `d3.json` to fetch the sample data for the plots
+    d3.json(`/metadata/${sample}`).then((data) => {
+      var otuIds = data.otu_ids;
+      var sampleValues = data.sample_values;
+      var otuLabels = data.otu_labels;
+
+
+    // @TODO: Build a Bubble Chart using the sample data
+    var bubbleLayout = {
+      xaxis: { title: "OTU ID" }
+    };
+    var bubbleData = [
+      {
+        x: otuIds,
+        y: sampleValues,
+        text: otuLabels,
+        mode: "markers",
+        marker: {
+          size: sampleValues,
+          color: otuIds,
+        }
+      }
+    ];
+
+    Plotly.plot("bubble", bubbleData, bubbleLayout);
+
+    // @TODO: Build a Pie Chart
+    // HINT: You will need to use slice() to grab the top 10 sample_values,
+    // otu_ids, and labels (10 each).
+    // Plot the default route once the page loads
+
+    var pieData = [
+      {
+        values: sampleValues.slice(0, 10),
+        labels: otuIds.slice(0, 10),
+        hovertext: otuLabels.slice(0, 10),
+        type: "pie"
+      }
+    ];
+
+    var pieLayout = {
+      margin: { t: 0, l: 0 }
+    };
+
+    Plotly.plot("pie", pieData, pieLayout);
   });
-//creating gauge chart via https://plot.ly/javascript/gauge-charts/
-d3.json(`/wfreq/${sample}`).then((sample)=>{
-  var wfreq = sample.WFREQ;
-  //created an empty variable f to hold the correct value to subtract since wash frequency is a range
-  var f;
-  if(wfreq===0){
-    f=15;
-  }else if(wfreq===1||wfreq===2){
-    f=45;
-  }else if(wfreq===3||wfreq===4){
-    f=75;
-  }else if(wfreq===5||wfreq===6){
-    f=105;
-  }else if(wfreq===7||wfreq===8){
-    f=135;
-  }else{f=165};
-
-  var degrees = 180- f,
-    radius = .5;
-
-  //
-  var radians = degrees * Math.PI / 180;
-  var x = radius * Math.cos(radians);
-  var y = radius * Math.sin(radians);
-  // Path: may have to change to create a better triangle
-  var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
-    pathX = String(x),
-    space = ' ',
-    pathY = String(y),
-    pathEnd = ' Z';
-  var path = mainPath.concat(pathX,space,pathY,pathEnd);
-  
-  var data = [{ type: 'scatter',
-    x: [0], y:[0],
-    marker: {size: 28, color:'850000'},
-    showlegend: false,
-    name: 'washes',
-    text: wfreq,
-    hoverinfo: 'text+name'},
-    { values: [50/6, 50/6, 50/6, 50/6, 50/6, 50/6, 50],
-    rotation: 90,
-    text: ['9+', '7-8', '5-6', '3-4',
-        '1-2', '0', ''],
-    textinfo: 'text',
-    textposition:'inside',	  
-    marker: {colors:['rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
-              'rgba(170, 202, 42, .5)', 'rgba(202, 209, 95, .5)',
-              'rgba(210, 206, 145, .5)', 'rgba(232, 226, 202, .5)',
-              'rgba(255, 255, 255, 0)']},
-    labels: ['9+', '7-8', '5-6', '3-4','1-2', '0', ''],
-    hoverinfo: 'label',
-    hole: .5,
-    type: 'pie',
-    showlegend: false
-  }];
-
-var layout = {
-  shapes:[{
-      type: 'path',
-      path: path,
-      fillcolor: '850000',
-      line: {
-        color: '850000'
-      }
-    }],
-  title: `frequency of sample washes for sample #${sample.sample}`,
-  height: 500,
-  width: 500,
-  xaxis: {zeroline:false, showticklabels:false,
-       showgrid: false, range: [-1, 1]},
-  yaxis: {zeroline:false, showticklabels:false,
-       showgrid: false, range: [-1, 1]}
-};
-
-Plotly.newPlot('gauge', data, layout)
 }
-)};  
 
 
 
